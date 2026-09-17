403855d32db57d7002b921350e78fc2135867b6b
gokulvamshi <gokulvamshi.workspace@gmail.com>
2026-09-17 15:21:34 +0530

    chore: untrack 1/.next artifacts, update .gitignore

--- 1/.next/dev/static/chunks/_1urspxg._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_globals_162hn9o.css","static/chunks/node_modules_motion-dom_dist_es_0hvf8rg._.js","static/chunks/node_modules_framer-motion_dist_es_1_a7n4k._.js","static/chunks/node_modules_@firebase_auth_dist_esm_0mecd1r._.js","static/chunks/node_modules_1oh8j09._.js","static/chunks/src_1k1iiep._.js","static/chunks/_1f0jghe._.js"],
-    source: "entry"
-});
